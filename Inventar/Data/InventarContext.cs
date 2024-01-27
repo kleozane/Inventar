2638a908adac7e9885ac002f31ecaa72756ef537
Kleo <kleozn@gmail.com>
2024-01-27 13:01:24 +0100

    Add project files.

---
+++ Inventar/Data/InventarContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Inventar.Data
+{
+    public class InventarContext : DbContext
+    {
+        public InventarContext(DbContextOptions<InventarContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Warehouse> Warehouses { get; set; }
+    }
+}
---
+++ Inventar/Migrations/20240121124904_AddWarehouseToDatabase.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inventar.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWarehouseToDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Warehouses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Warehouses", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Warehouses");
+        }
+    }
+}
